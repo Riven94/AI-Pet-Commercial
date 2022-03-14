d0e83edb3e8d4be1e7d12377e161091a03e29680
Riven94 <57120419+Riven94@users.noreply.github.com>
2022-03-14 11:51:16 +0800

    fix: uploadImage interface

--- pages/knowledge/addarticle/index.js
+++ pages/knowledge/addarticle/index.js
@@ -32,145 +32,142 @@ Page({
       Type: that.data.selectDatas[Indexs]
     });
     console.log(that.data.Type)
+  },
+
+  title:function(e){
+    this.setData({
+        articletitle:e.detail.value
+      })   
+  },
+
+  content:function(e){
+    this.setData({
+        Content:e.detail.value
+      })   
+  },
+
+  uploadImage:function(){
+    var that=this;
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success (res) {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths 
+        console.log(res)
+        that.setData({
+            imageList:res.tempFilePaths
+        })
+        console.log("aaaaaaaaa",that.data.imageList)
+        that.upload({
+          path: tempFilePaths
+      })
+      }
+    })
+  },
+
+  upload(data) { // 上传图片
+    var that = this;
+    wx.showToast({
+        icon: "loading",
+        title: "正在上传"
+    }),
+    wx.request({
+        //上传图片协议接口
+        url: domain+'/iamges/uploadFile/article ',
+        method: 'POST',
+        data: {
+          "img": that.data.imageList,
+          "creatorId": 1
+        },
+        header:{ 'content-type': 'multipart/form-data'},
+        success(res) {
+          console.log("上传图片成功")
+        },
+        fail(e) {
+          wx.showModal({
+              title: '提示',
+              content: '上传失败',
+              showCancel: false
+          })
+        },
+    })
+  },
+
+  bindSumbit:function(params){
+    var that=this;
+    wx.request({
+        url:domain+ '/knowledge/upload ', 
+        method:"POST",
+        data: {
+          "creatorId":1,
+          "articleTitle":that.data.articletitle,
+          "article":that.data.Content,
+          "imgUrl":that.data.imageList,
+          "type":that.data.Type
+        },
+        header: {
+            "content-type": "application/x-www-form-urlencoded"
+        },
+        success (res) {
+          console.log(res.data)
+        }
+      })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
 
   },
-      title:function(e){
-        this.setData({
-            articletitle:e.detail.value
-          })   
-      },
-      content:function(e){
-        this.setData({
-           Content:e.detail.value
-          })   
-      },
-      uploadImage:function(){
-        var that=this;
-        wx.chooseImage({
-          count: 5,
-          sizeType: ['original', 'compressed'],
-          sourceType: ['album', 'camera'],
-          success (res) {
-            // tempFilePath可以作为img标签的src属性显示图片
-            const tempFilePaths = res.tempFilePaths 
-            console.log(res)
-            that.setData({
-                imageList:res.tempFilePaths
-            })
-            console.log("aaaaaaaaa",that.data.imageList)
-            that.upload({
-              path: tempFilePaths
-          })
-          }
-       
-        })
-    },
-    upload(data) { // 上传图片
-        var that = this;
-        wx.showToast({
-                icon: "loading",
-                title: "正在上传"
-            }),
-        wx.request({
-            //上传图片协议接口
-            url: domain+'   /iamges/uploadFile/article ',
-            method: 'POST',
-            data: {
-              "img": that.data.imageList,
-              "creatorId": 1
-            },
-            header: {
-                "content-type": "application/x-www-form-urlencoded"
-            },
-            success(res) {
-            console.log("上传图片成功")
 
-            },
-            fail(e) {
-                wx.showModal({
-                    title: '提示',
-                    content: '上传失败',
-                    showCancel: false
-                })
-            },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
 
-        })
-    },
-    bindSumbit:function(params){
-        var that=this;
-        wx.request({
-            url:domain+ '/knowledge/upload ', 
-           method:"POST",
-            data: {
-             "creatorId":1,
-             "articleTitle":that.data.articletitle,
-             "article":that.data.Content,
-             "imgUrl":that.data.imageList,
-             "type":that.data.Type
-            },
-            
-            header: {
-                "content-type": "application/x-www-form-urlencoded"
-            },
-            success (res) {
-              console.log(res.data)
-            }
-          })
+  },
 
-    },
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
 
-    },
+  },
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
 
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
+  }
 })
